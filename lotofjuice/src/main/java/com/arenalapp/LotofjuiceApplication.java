3c2df04c9128b37cd5ad4783df8f851495134ac1
Mario Salvatierra <mesalvav@yahoo.com>
2017-11-06 08:09:52 -0500

    PRIMER COMMITMENT

---
+++ lotofjuice/src/main/java/com/arenalapp/LotofjuiceApplication.java
@@ -0,0 +1,12 @@
+package com.arenalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotofjuiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotofjuiceApplication.class, args);
+	}
+}
